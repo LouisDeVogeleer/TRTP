9f810ac8887385755f0962134ec57430fc2136b3
LouisDeVogeleer <louis.devogeler@student.uclouvain.be>
2018-10-19 15:18:53 +0200

    interface 2 fonctions

--- src/receiver.c
+++ src/receiver.c
@@ -1,10 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 
+
+//pas oublier de free : file, host
 int main(int argc, char *argv[]){
-	struct record {
-		int header;
-		int * payload;
-		int footer;
+	int isOutFile = 0;         /*  Si = 1, le payload ira vers file. Sinon, le payload vient de STDOUT.*/
+	char * file;
+	//char * payload;
+	int isAllInt = 0;      /*  Si = 1, on ecoute toutes les interfaces . Sinon, l'hote est précisé dans host.*/
+	char * host;
+	int port;
+	
+	for(int i = 1; i < argc; i++){
+		if(isOutFile == 0 && strcmp(argv[i], "-f") == 0){
+			isOutFile = 1;
+			int lenfile = strlen(argv[i+1]);
+			file = (char *) malloc(lenfile*sizeof(char));
+			for(int j=0; j<lenfile; j++){
+				file[j] = argv[i+1][j];
+			}
+			i++;
+		}
+				
+		else if(isAllInt == 0 && strcmp(argv[i], "::") == 0){
+			isAllInt = 1;
+		}
+		else if(isAllInt == 0 && host == NULL) {
+			int lenhost = strlen(argv[i]);
+			host = (char *) malloc(lenhost*sizeof(char));
+			for(int j=0; j<lenhost; j++){
+				host[j] = argv[i][j];
+				}
+		}
+		else{
+			port = atoi(argv[i]);
+		}
 	}
+	
+	printf("-> isOutFile : %d\n", isOutFile);
+	if(isOutFile == 1) printf("-> file : %s\n", file);
+	printf("-> isAllInt : %d\n", isAllInt);
+	if(isAllInt == 0) printf("-> host : %s\n", host);
+	printf("-> port : %d\n", port);
 }
+	
--- src/sender.c
+++ src/sender.c
@@ -1,16 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 
+
+//pas oublier de free : file, host
 int main(int argc, char *argv[]){
-	int op;
-	int isErr;                /*  si vaut 1, ecrit les messages d'erreur sur le fichier errFile*/
-	int isSTDout
-	char * errFile;
-	int isSource;         /*  si vaut 1, le payload vient de sourceFile. si vaut 0, le payload vient de STDIN*/
-	char * sourceFile;
-	char * payload;
-	while( (op = getopt(argc, argv, "")) != -1){
+	int isInFile = 0;         /*  Si = 1, le payload vient de file. Sinon, le payload vient de STDIN.*/
+	char * file;
+	//char * payload;
+	int isLocal = 0;      /*  Si = 1, l'hote est local. Sinon, il est précisé dans host.*/
+	char * host;
+	int port;
 		
+	for(int i = 1; i < argc; i++){
+		if(isInFile == 0 && strcmp(argv[i], "-f") == 0){
+			isInFile = 1;
+			int lenfile = strlen(argv[i+1]);
+			file = (char *) malloc(lenfile*sizeof(char));
+			for(int j=0; j<lenfile; j++){
+				file[j] = argv[i+1][j];
+			}
+			i++;
+		}
+				
+		else if(isLocal == 0 && strcmp(argv[i], "::1") == 0){
+			isLocal = 1;
+		}
+		else if(isLocal == 0 && host == NULL) {
+			int lenhost = strlen(argv[i]);
+			host = (char *) malloc(lenhost*sizeof(char));
+			for(int j=0; j<lenhost; j++){
+				host[j] = argv[i][j];
+				}
+		}
+		else{
+			port = atoi(argv[i]);
+		}
 	}
 	
+	printf("-> isInFile : %d\n", isInFile);
+	if(isInFile == 1) printf("-> file : %s\n", file);
+	printf("-> isLocal : %d\n", isLocal);
+	if(isLocal == 0) printf("-> host : %s\n", host);
+	printf("-> port : %d\n", port);
 }
+	
