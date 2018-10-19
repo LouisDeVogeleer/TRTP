25c30fc31bb49480f51c5434353796312013b07f
LouisDeVogeleer <louis.devogeler@student.uclouvain.be>
2018-10-19 10:36:27 +0200

    ajout de sender.c et receiver.c

---
+++ src/sender.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int op;
+	int isErr;                /*  si vaut 1, ecrit les messages d'erreur sur le fichier errFile*/
+	int isSTDout
+	char * errFile;
+	int isSource;         /*  si vaut 1, le payload vient de sourceFile. si vaut 0, le payload vient de STDIN*/
+	char * sourceFile;
+	char * payload;
+	while( (op = getopt(argc, argv, "")) != -1){
+		
+	}
+	
+}
